bbeefbf9ae97878e179f53cc6fbe111a7e88b689
Austin Haedicke <austin.haedicke@gmail.com>
2020-05-10 07:04:26 -0400

    new post: stoic reflection 021, carnivore75hard week 03

---
+++ docs/alpha/script-corr.r
@@ -0,0 +1,46 @@
+#!/usr/bin/R
+# compute descriptive statistics, correlation, and regression from raw.txt
+
+# required pacakges
+# - dplyr
+# - Hmisc
+# - corrplot
+
+# library
+library("dplyr")
+library("Hmisc")
+library("corrplot")
+
+# improt raw .txt to data frame
+raw_data <- read.delim("raw_data-test.txt")
+
+# convert data to numerical matrix
+df <- as.matrix(as.data.frame(lapply(raw_data, as.numeric)))
+
+# output file - descriptive stats
+sink("descriptive_stats-test.txt", split=TRUE, append=FALSE)
+
+# descriptive stats
+summary(raw_data)
+
+# output file - correlation and regression
+sink("correlation-test.txt", split=TRUE, append=FALSE)
+
+# correlations (r) and significance (p)
+rcorr(df, type=c("pearson"))
+
+# plotting correlation (insignificant left blank)
+res <- rcorr(df, type=c("pearson"))
+corrplot(res$r, type="upper", p.mat = res$P, sig.level = 0.05, insig = "blank")
+dev.copy(png, "test-corr-050.png")
+dev.off()
+
+# repeat at 4 = 0.01
+corrplot(res$r, type="upper", p.mat = res$P, sig.level = 0.01, insig = "blank")
+dev.copy(png, "test-corr-010.png")
+dev.off()
+
+# repeat at r = 0.001
+corrplot(res$r, type="upper", p.mat = res$P, sig.level = 0.001, insig = "blank")
+dev.copy(png, "test-corr-001.png")
+dev.off()
