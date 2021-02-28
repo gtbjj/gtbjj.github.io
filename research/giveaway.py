2f760e4125815251d6263f803fe1f96cae0d9b59
savagezen <austin.haedicke@gmail.com>
2021-02-28 15:05:47 -0500

    research: giveaway: february contestants and winners

--- research/giveaway.py
+++ research/giveaway.py
@@ -2,20 +2,20 @@
 
 import random
 
-# 1 entry
-post_likes = ["name1", "name2", "name3"]
+# 1 entry / challenge post like
+post_likes = ["burberry_venom", "hotchossbuns", "heidi.cross_carni.keto", "b_mac_37", "kratomkalbar_indonesia","tsunamitheseawing87", "ana_beatriz1971", "almacomunidad", "instinctivemovement", "instintiva_mente", "gentlezipper", "lifeontheout", "meat.your.potential", "ralph_ospina", "diabadassbeth"]
 
 # 3 entries
-telegram_members = ["name4", "name5"]
+telegram_members = ["Rick White", "Zach Moore", "Melvin Chia", "Rita Haedicke", "Hannah", "Joe Balderamma", "Cameron Turner", "Shaina", "Frank Lampard"]
 telegram = telegram_members + telegram_members + telegram_members
 
 # 5 entries
-podcast_reviewers = ["name6"]
+podcast_reviewers = []
 podcast = podcast_reviewers + podcast_reviewers + podcast_reviewers + podcast_reviewers + podcast_reviewers
 
 entries = post_likes + telegram + podcast
 winners = random.sample(entries, 2)
 print(winners)
 
-
-# February Winners:
+# 2021 Winners:
+# February: Rita Haedicke (telegram), diabadassbeth (instagram)
